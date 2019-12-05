6aa04931cf589b6b2d14dd5bbc82af5a4f865c0a
Nils Dunkel <InimicusD@googlemail.com>
2019-12-05 13:03:01 +0100

    Add project.

---
+++ ContextActionRepro/ContextActionRepro/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ContextActionRepro
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private int _tapCount = 0;
+
+		private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			_tapCount++;
+			tapCount.Text = "Taps on Items: " + _tapCount;
+		}
+	}
+}
---
+++ ContextActionRepro/ContextActionRepro/MainPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ContextActionRepro
+{
+	public class MainPageViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected bool SetProperty<T>(ref T storage, T value,
+									  [CallerMemberName] string propertyName = null)
+		{
+			if (Object.Equals(storage, value))
+				return false;
+
+			storage = value;
+			OnPropertyChanged(propertyName);
+			return true;
+		}
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		private Command _contextCommand;
+		public ICommand ContextCommand => _contextCommand ?? (_contextCommand = new Command(ContextMethod, CanContextCommand));
+
+		private bool CanContextCommand(object arg)
+		{
+			return false;
+		}
+
+		private void ContextMethod(object obj)
+		{
+			// ...
+		}
+	}
+}
